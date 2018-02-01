edafb72e6b6d1cfbfccaf87908b66bfeb3d8a017
Eder <chartrix@gmail.com>
2018-02-01 16:38:43 -0600

    New name fix

--- ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
@@ -3,7 +3,7 @@ package com.mx.datio
 object App {
   
   def main(args : Array[String]) {
-    val nombre = "PEPE"
+    val nombre = "El senor de los cielos"
     println( s"Hola Clase git saludos de $nombre" )
   }
 
