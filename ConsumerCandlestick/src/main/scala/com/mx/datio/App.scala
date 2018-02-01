8d8a6ad6b6b83f67d771ef9f6bfc8be22dceac9c
Eder <chartrix@gmail.com>
2018-02-01 15:35:47 -0600

    cambiando el nombre del desarrollador de develop

--- ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
@@ -3,7 +3,7 @@ package com.mx.datio
 object App {
   
   def main(args : Array[String]) {
-    val nombre = "Eder"
+    val nombre = "PEPE"
     println( s"Hola Clase git saludos de $nombre" )
   }
 
