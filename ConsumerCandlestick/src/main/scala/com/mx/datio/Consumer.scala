ad74de2d426e4da39d086b5f3ada449869454f71
Eder <chartrix@gmail.com>
2018-02-02 00:02:24 -0600

    [C2] Parametro deprecado en clase Consumer

--- ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
@@ -1,8 +1,7 @@
 package com.mx.datio
 
-class Consumer(url: String, speed: Int, timy: Float){
+class Consumer(url: String, speed: Int){
     var urlData:    String = url
     var speedData:  Int    = speed
-    var timeStop:   Float  = timy
     override def toString(): String = "(" + urlData + "," + speedData + ")"
 }
