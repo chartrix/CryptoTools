afd762e9f8ce551adf35603aa18036e33269cdaa
Eder <chartrix@gmail.com>
2018-02-01 23:31:19 -0600

    [C2] Cambio de valor en variable name de App

--- ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
@@ -3,7 +3,7 @@ package com.mx.datio
 object App {
   
   def main(args : Array[String]) {
-    val nombre = "El senor de los cielos"
+    val nombre = "El senor de los cielos e infiernos"
     println( s"Hola Clase git saludos de $nombre" )
   }
 
