8780efde62a5cd52905c4ea2f1aa3f84fbbd0899
Eder <chartrix@gmail.com>
2018-02-02 01:29:24 -0600

    [C2] Modificando el nombre a Africano

--- ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/App.scala
@@ -3,7 +3,7 @@ package com.mx.datio
 object App {
   
   def main(args : Array[String]) {
-    val nombre = "Poloniex"
+    val nombre = "Africano"
     println( s"Hola Clase git saludos de $nombre" )
   }
 
