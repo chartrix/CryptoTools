8fae2107b9d135e2b3690d40a337f09b4689dcd0
Eder <chartrix@gmail.com>
2018-02-02 01:35:40 -0600

    [C4] Cambie el formato de toString() a un formato cool

--- ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
@@ -3,5 +3,5 @@ package com.mx.datio
 class Consumer(url: String, speed: Int){
     var urlData:    String = url
     var speedData:  Int    = speed
-    override def toString(): String = "(" + urlData + "," + speedData + ")"
+    override def toString(): String = "-------------->(" + urlData + "," + speedData + ")<------------------"
 }
