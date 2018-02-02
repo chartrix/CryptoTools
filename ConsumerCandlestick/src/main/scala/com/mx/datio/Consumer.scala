83cb090a3ffc24a47f9898465c7b0a045017d656
Eder <chartrix@gmail.com>
2018-02-01 23:29:48 -0600

    [C1] Nueva propiedad a la clase Consumer

--- ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
+++ ConsumerCandlestick/src/main/scala/com/mx/datio/Consumer.scala
@@ -1,8 +1,8 @@
 package com.mx.datio
 
-class Consumer(url: String, speed: Int){
+class Consumer(url: String, speed: Int, timy: Float){
     var urlData:    String = url
     var speedData:  Int    = speed
-
+    var timeStop:   Float  = timy
     override def toString(): String = "(" + urlData + "," + speedData + ")"
 }
